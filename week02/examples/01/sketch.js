43b90a8c1da77c7b2ebae3a6132c8654cef6b106
Jin Sung Yoo <ujinsung@gmail.com>
2017-01-13 16:05:05 -0500

    change work folder structure.

--- week02/examples/01/sketch.js
+++ week02/examples/01/sketch.js
@@ -1,14 +1,11 @@
 function setup () {
   createCanvas(640, 640);
-  background(0);
+
 }
 
 function draw() {
-  var rand = random(10);
-  fill (rand * 10 + 100);
-  ellipse(width/2 + rand, height/2 + rand, mouseX, mouseY);
-}
-
-function mousePressed() {
-  saveFrame();
+  background(0);
+  fill (100);
+  ellipse(width/2, height/2, 100, 100);
+  // draw rect?
 }
