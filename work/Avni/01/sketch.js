d902719435e1720ac8374e110560fbd031c8556f
Avni Jain <ajain4@sva.edu>
2017-01-20 20:47:53 -0500

    Add Avni’s folder

---
+++ work/Avni/01/sketch.js
@@ -0,0 +1,13 @@
+function setup () {
+	createCanvas(640, 480);
+	background(250, 0, 0);
+    rectMode(CENTER);
+	stroke(255, 0, 0);
+	noStroke();
+	fill(0, 0, 255);
+	ellipse(320,240,100,100);
+	fill(0, 255, 0);
+	ellipse(100,100,100,50);
+
+	rect(100,100,100,100);
+}
