075fc88e7dc6bd4474f6e1fb6c41cdbccd9bf6c7
Jin Sung Yoo <ujinsung@gmail.com>
2017-01-25 21:03:13 -0500

    add examples for week 3

---
+++ week03/examples/02_variables/sketch.js
@@ -0,0 +1,28 @@
+var xPosition = 320;
+var yPosition = 240
+function setup() {
+  createCanvas(640, 480);
+  noStroke();
+}
+
+function draw() {
+  background(100);
+  ellipseMode(CENTER);
+  
+  fill (240, 0, 0);
+  ellipse(xPosition, yPosition, 100, 100);
+
+  fill (220, 0, 0);
+  ellipse(xPosition, yPosition, 80, 80);
+
+  fill (200, 0, 0);
+  ellipse(xPosition, yPosition, 60, 60);
+
+  fill (180, 0, 0);
+  ellipse(xPosition, yPosition, 40, 40);
+
+  fill (160, 0, 0);
+  ellipse(xPosition, yPosition, 20, 20);
+
+  xPosition = xPosition + 1;
+}
