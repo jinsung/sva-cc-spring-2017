2e55860d32c44cd7aeb439a9c1afd02587c1adce
Jin Sung Yoo <ujinsung@gmail.com>
2017-01-31 10:00:07 -0500

    add svg export example

---
+++ work/QandA/svg-export/sketch.js
@@ -0,0 +1,36 @@
+// Example 4-7: Filling variables with random values
+
+var r;
+var g;
+var b;
+var a;
+
+var size;
+var x;
+var y;
+
+function setup() {
+  createCanvas(640, 480, SVG);
+  background(0);
+}
+
+function draw() {
+  // Each time through draw(), new random
+  // numbers are picked for a new ellipse.
+  r = random(255);
+  g = random(255);
+  b = random(255);
+  a = random(255);
+  size = random(20);
+  x = random(width);
+  y = random(height);
+
+  // Use values to draw an ellipse
+  noStroke();
+  fill(r, g, b, a);
+  ellipse(x, y, size, size);
+}
+
+function mouseReleased() {
+  save();
+}
