213d09f061d42780c924eb8fbb7bb20b7ed54aa1
Jin Sung Yoo <ujinsung@gmail.com>
2017-02-22 00:18:44 -0500

    add examples for week 6

---
+++ week06/examples/04_kimberly_obj_robot/robot.js
@@ -0,0 +1,20 @@
+function Robot() {
+
+
+  this.draw = function (isOn) {
+    stroke(0);
+    fill(100);
+    rectMode(CENTER);
+    ellipseMode(CENTER);
+    this.drawHead();
+    this.drawBody();
+  }
+
+  this.drawHead = function() {
+
+  }
+
+  this.drawBody = function () {
+
+  }
+}
