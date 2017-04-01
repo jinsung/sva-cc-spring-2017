7967f7c7cc929d6d20767644e2f83beecb8e98e3
Jin Sung Yoo <ujinsung@gmail.com>
2017-03-31 20:25:21 -0400

    WiP

--- work/jinsung/week10/02_asteroid/asteroid.js
+++ work/jinsung/week10/02_asteroid/asteroid.js
@@ -1,10 +1,11 @@
 function Asteroid () {
-	this.setup = function (loc) {
-		this.size = 50;
+	this.setup = function (loc, size) {
+		this.size = size;
 		this.maxSpeed = 3;
 		this.position = createVector(loc.x, loc.y);
 		this.velocity = createVector(0, 0);
 		this.acc = createVector(0, 0);
+		this.isDead = false;
 	};
 
 	this.addForce = function (vector) {
@@ -15,6 +16,15 @@ function Asteroid () {
 		this.velocity.add(this.acc);
 		this.position.add(this.velocity);
 		this.velocity.limit(this.maxSpeed);
+
+		if (this.position.x < 0 || this.position.x > width ||
+			this.position.y < 0 || this.position.y > height) {
+			this.isDead = true;
+		}
+	};
+
+	this.hit = function () {
+
 	};
 
 	this.draw = function () {
