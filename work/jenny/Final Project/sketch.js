1d9a1f81e9e8a751e1e6442c68f7cdb97223706c
hlee89 <heejinlee@Heejins-MBP.home>
2017-04-30 23:57:13 -0400

    20170430 Final
    Thank you Jin! :)

--- work/jenny/Final Project/sketch.js
+++ work/jenny/Final Project/sketch.js
@@ -1,6 +1,7 @@
 var allParticles = [];
 var maxLevel = 3;
 var useFill = false;
+var colorChange =160;
 
 var data = [];
 
@@ -13,21 +14,21 @@ function Particle(x, y, level) {
   
   this.pos = new p5.Vector(x, y);
   this.vel = p5.Vector.random2D();
-  this.vel.mult(map(this.level, 0, maxLevel, 5, 2));
+  this.vel.mult(map(this.level, 0, maxLevel, 3, 2));
   
   this.move = function() {
     this.life++;
     
     // Add friction.
-    this.vel.mult(0.913);
+    this.vel.mult(0.93);
     
     this.pos.add(this.vel);
     
     // Spawn a new particle if conditions are met.
     if (this.life % 10 == 0) {
       if (this.level > 0) {
-        this.level -= 1;
-        var newParticle = new Particle(this.pos.x, this.pos.y, this.level-1);
+        this.level -= 2;
+        var newParticle = new Particle(this.pos.x, this.pos.y, this.level-2);
         allParticles.push(newParticle);
       }
     }
@@ -38,18 +39,19 @@ function Particle(x, y, level) {
 function setup() {
   createCanvas(windowWidth, windowHeight); 
   
-  colorMode(HSB, 410);
+  colorMode( HSB, 400);
   
   textAlign(CENTER);
   
   background(0);
+
 } 
 
 
 function draw() {
   // Create fade effect.
   noStroke();
-  fill(0, 30);
+  fill(0, 50);
   rect(0, 0, width, height);
   
   // Move and spawn particles.
@@ -68,7 +70,7 @@ function draw() {
       return [pt.pos.x, pt.pos.y];
     }));
   	
-    strokeWeight(0.1);
+    strokeWeight(0.05);
     
     // Display triangles individually.
     for (var i = 0; i < data.length; i += 3) {
@@ -78,7 +80,7 @@ function draw() {
       var p3 = allParticles[data[i+2]];
       
       // Don't draw triangle if its area is too big.
-      var distThresh = 2000;
+      var distThresh = 700;
       
       if (dist(p1.pos.x, p1.pos.y, p2.pos.x, p2.pos.y) > distThresh) {
         continue;
@@ -95,10 +97,10 @@ function draw() {
       // Base its hue by the particle's life.
       if (useFill) {
         noStroke();
-        fill(165+p1.life*1.5, 360, 360);
+        fill(colorChange+p1.life*1.5, 360, 360);
       } else {
         noFill();
-        stroke(165+p1.life*1.5, 360, 360);
+        stroke(colorChange+p1.life*1.5, 360, 360);
       }
       
       triangle(p1.pos.x, p1.pos.y, 
@@ -109,7 +111,10 @@ function draw() {
   
   noStroke();
   fill(255);
-  text("Move your mouse in the window\nPress any key to change to fill/stroke", width/2, height-50);
+  text("Move your mouse in the window\nPress any key to change to fill/stroke\nClick to change colors", width/2, height-40);
+    
+    
+
 }
 
 
@@ -121,3 +126,18 @@ function mouseMoved() {
 function keyPressed() {
   useFill = ! useFill;
 }
+
+function mousePressed(){
+   colorChange++;
+    if (colorChange < 360){
+        colorChange = 0;
+    } else {
+        colorChange = 205;
+    }
+    
+    //return colorChange;
+    
+}
+
+
+
